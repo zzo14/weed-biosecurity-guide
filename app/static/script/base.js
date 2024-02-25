3e2cdee6aa29305474297e3ff25cc8258fb0fd00
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-02-25 17:54:30 +1300

    Update base.js
    add one function for toggling weed manage button. another for previewing uploaded image

--- app/static/script/base.js
+++ app/static/script/base.js
@@ -16,4 +16,42 @@ function togglePasswordVisibility(password_id) {
     passwordInput.type = isVisibility ? 'password' : 'text';
     var toggle_icon = passwordInput.nextElementSibling.querySelector('i');
     toggle_icon.className = isVisibility ? 'fa-regular fa-eye' : 'fa-regular fa-eye-slash';
-}
+};
+
+function toggleManageButton() {
+    var manageButton = document.querySelectorAll('.manage-buttons');
+    manageButton.forEach(function(button) {
+        if (button.classList.contains('d-none')) {
+            button.classList.remove('d-none');
+        } else {
+            button.classList.add('d-none');
+        }
+    });
+};
+
+function previewImg(event) {
+    var files = event.target.files;
+    var imgPreviewContainer = document.getElementById('image-preview-container');
+    imgPreviewContainer.innerHTML = '';
+
+    for (var i = 0; i < files.length; i++) {
+        if (!files[i].type.match('image.*')) {
+            continue;
+        }
+        var reader = new FileReader();
+        reader.onload = (function(theFile) {
+            return function(e) {
+                var imgDiv = document.createElement('div');
+                imgDiv.classList.add('col-3', 'my-2');
+
+                var img = document.createElement('img');
+                img.classList.add('img-fluid', 'rounded', 'border', 'shadow-sm');
+                img.src = e.target.result;
+                img.alt = theFile.name;
+                imgDiv.appendChild(img);
+                imgPreviewContainer.appendChild(imgDiv);
+            };
+        })(files[i]);
+        reader.readAsDataURL(files[i]);
+    }
+};
