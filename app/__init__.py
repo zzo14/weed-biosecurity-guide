4883f5a6bb8796349f5930a3698a42a90bb77614
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-03-01 12:51:12 +1300

    Update __init__.py
    Refactor code as following blueprint structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from datetime import timedelta
+import os
 
-app = Flask(__name__)
 
-from app import views
+def create_app():
+    app = Flask(__name__)
+
+    from app.view import home_view, auth_view, user_view, admin_staff_view, weed_view
+    app.secret_key = 'key'
+    app.permanent_session_lifetime = timedelta(hours=24)
+    UPLOAD_FOLDER = os.path.join(app.root_path, 'static/images/db')
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+    app.register_blueprint(home_view.home_bp)
+    app.register_blueprint(auth_view.auth, url_prefix='/auth')
+    app.register_blueprint(user_view.user, url_prefix='/user')
+    app.register_blueprint(admin_staff_view.admin_staff, url_prefix='/admin_staff')
+    app.register_blueprint(weed_view.weed, url_prefix='/weed')
+
+    return app
