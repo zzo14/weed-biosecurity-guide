9539d2742d536fc4be82ffab4a7d562e1198cba2
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-02-29 21:25:43 +1300

    Update views.py
    implement back end codes for manage staff function (view, add, update and delete) as a admin role. And Merge code to reduce duplicate codes

--- app/views.py
+++ app/views.py
@@ -156,24 +156,20 @@ def update_gardener_profile(gardener_id):
         
         try: 
             if gardener_id:
-                query = "UPDATE gardener SET first_name=%s, last_name=%s, address=%s, email=%s, phone_number=%s WHERE gardener_id = %s "
-                connection.execute(query, (first_name, last_name, address, email, phone_number, gardener_id))
-                affected_rows = connection.rowcount
-                if affected_rows > 0:
-                    flash("Successfully update! ", "success")
-                    return redirect(url_for('gardener_list'))
-                else:
-                    flash("Update failed. Please try again.", "danger")
-                    return redirect(url_for('gardener_list'))
+                target_id = gardener_id
+                direct_to = url_for('gardener_list')
             else:
-                query = "UPDATE gardener SET first_name=%s, last_name=%s, address=%s, email=%s, phone_number=%s WHERE gardener_id = %s "
-                connection.execute(query, (first_name, last_name, address, email, phone_number, id))
-                affected_rows = connection.rowcount
-                if affected_rows > 0:
-                    flash("Successfully update! ", "success")
-                    return redirect(url_for('gardener_profile'))
-                else:
-                    flash("Update failed. Please try again.", "danger")
+                target_id = id
+                direct_to = url_for('gardener_profile')
+            query = "UPDATE gardener SET first_name=%s, last_name=%s, address=%s, email=%s, phone_number=%s WHERE gardener_id = %s "
+            connection.execute(query, (first_name, last_name, address, email, phone_number, target_id))
+            affected_rows = connection.rowcount
+            if affected_rows > 0:
+                flash("Successfully update! ", "success")
+                return redirect(direct_to)
+            else:
+                flash("Update failed. Please try again.", "danger")
+                return redirect(direct_to)
         except Exception as e:
             flash(f"Error: {e}. Update failed. Please try again.", "danger")
     return redirect(url_for('gardener_profile'))
@@ -212,8 +208,9 @@ def admin_profile():
     else:
         return redirect(url_for('home'))
 
-@app.route('/update_SA_profile', methods=['GET', 'POST'])
-def update_SA_profile():
+@app.route('/update_SA_profile', defaults={'staff_id': None}, methods=['GET', 'POST'])
+@app.route('/update_SA_profile/<int:staff_id>', methods=['GET', 'POST'])
+def update_SA_profile(staff_id):
     connection = getCursor()
     id = session['id']
     usertype = session['userType']
@@ -229,18 +226,26 @@ def update_SA_profile():
         if not (first_name and last_name and work_phone and email and position and department):
             flash("Please fill out the form!", "danger")
             return redirect(profile_url)
+        
         try:
+            target_id = id
+            direct_to = profile_url
+            query = "UPDATE staff SET first_name=%s, last_name=%s, email=%s, work_phone=%s, position=%s, department=%s WHERE staff_id = %s "
             if usertype == 'Admin':
                 query = "UPDATE administrator SET first_name=%s, last_name=%s, email=%s, work_phone=%s, position=%s, department=%s WHERE admin_id = %s "
-            else:
-                query = "UPDATE staff SET first_name=%s, last_name=%s, email=%s, work_phone=%s, position=%s, department=%s WHERE staff_id = %s "
-            connection.execute(query, (first_name, last_name, email, work_phone, position, department, id))
+                if staff_id:
+                    target_id = staff_id
+                    direct_to = url_for('staff_list')
+                    query = "UPDATE staff SET first_name=%s, last_name=%s, email=%s, work_phone=%s, position=%s, department=%s WHERE staff_id = %s "
+
+            connection.execute(query, (first_name, last_name, email, work_phone, position, department, target_id))
             affected_rows = connection.rowcount
             if affected_rows > 0:
                 flash("Successfully update! ", "success")
-                return redirect(profile_url)
+                return redirect(direct_to)
             else:
                 flash("Update failed. Please try again.", "danger")
+
         except Exception as e:
             flash(f"Error: {e}. Update failed. Please try again.", "danger")
     return redirect(profile_url)
@@ -313,20 +318,6 @@ def delete_gardener(gardener_id):
             flash("Delete failed. Please try again.", "danger")
     return redirect(url_for('gardener_list'))
 
-@app.route('/staff_list')
-def staff_list():
-    if 'loggedin' in session and session['userType'] == 'Admin':
-        connection = getCursor()
-        query = "SELECT * FROM staff"
-        connection.execute(query)
-        staff_list = connection.fetchall()
-        active_staffs, inactive_staffs = handle_user_data(staff_list)
-        return render_template("staff_list.html", username=session['username'], userType=session['userType'], profile_url=url_for('admin_profile'), active_staffs=active_staffs, inactive_staffs=inactive_staffs)
-    else:
-        flash("Illegal Access!", "danger")
-        return redirect(url_for('home'))
-
-
 @app.route('/gardener_list/recover_gardener_account/<int:gardener_id>',methods=['GET', 'POST'])
 def recover_gardener_account(gardener_id):
     connection = getCursor()
@@ -341,6 +332,87 @@ def recover_gardener_account(gardener_id):
             flash("Recover failed. Please try again.", "danger")
     return redirect(url_for('gardener_list'))
 
+@app.route('/staff_list')
+def staff_list():
+    if 'loggedin' in session and session['userType'] == 'Admin':
+        connection = getCursor()
+        query = "SELECT * FROM staff"
+        connection.execute(query)
+        staff_list = connection.fetchall()
+        active_staffs, inActive_staffs = handle_user_data(staff_list)
+        return render_template("staff_list.html", username=session['username'], userType=session['userType'], profile_url=url_for('admin_profile'), active_staffs=active_staffs, inActive_staffs=inActive_staffs)
+    else:
+        flash("Illegal Access!", "danger")
+        return redirect(url_for('home'))
+
+@app.route('/staff_list/add_new_staff',  methods=['GET', 'POST'])
+def add_new_staff():
+    connection = getCursor()
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        work_phone = request.form.get('work_phone')
+        email = request.form.get('email')
+        position = request.form.get('position')
+        department = request.form.get('department')
+
+        if not (username and password and first_name and last_name and work_phone and email and position and department):
+            flash("Please fill out the form!", "danger")
+            return redirect(url_for('add_new_staff'))
+
+        if not re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$', password):
+            flash("Password must be at least 8 characters long and conatin uppercase, lowercase, number and special characters.")
+            return redirect(url_for('add_new_staff'))
+
+        hashed_password = generate_password_hash(password) 
+
+        try:
+            query = "INSERT INTO userAuth (username, password_hash, userType) VALUES (%s, %s, %s)"
+            connection.execute(query, (username, hashed_password, 'Staff'))
+            new_id = connection.lastrowid
+            affected_rows = connection.rowcount
+            if new_id and affected_rows > 0:
+                query = "INSERT INTO staff (staff_id, username, first_name, last_name, email, work_phone, hire_date, position, department, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                connection.execute(query, (new_id, username, first_name, last_name, email, work_phone, datetime.now(), position, department, 'Active'))
+                affected_rows = connection.rowcount
+                if affected_rows > 0:
+                    flash("Successfully add a new staff! ", "success")
+                    return redirect(url_for('staff_list'))
+            else:
+                flash("Username already exists, please try another one.", "danger")
+        except Exception as e:
+            flash(f"Error: {e}. Add failed. Please try again.", "danger")
+    return redirect(url_for('add_new_staff'))
+
+@app.route('/staff_list/delete_staff/<int:staff_id>', methods=['GET', 'POST'])
+def delete_staff(staff_id):
+    connection = getCursor()
+    if request.method == 'POST':
+        query = "UPDATE staff SET status='Inactive' WHERE staff_id = %s"
+        connection.execute(query, (staff_id,))
+        affected_rows = connection.rowcount
+        if affected_rows > 0:
+            flash("Successfully delete the gardener!", "success")
+            return redirect(url_for('staff_list'))
+        else:
+            flash("Delete failed. Please try again.", "danger")
+    return redirect(url_for('staff_list'))
+
+@app.route('/staff_list/recover_staff_account/<int:staff_id>',methods=['GET', 'POST'])
+def recover_staff_account(staff_id):
+    connection = getCursor()
+    if request.method == 'POST':
+        query = "UPDATE staff SET status='Active' WHERE staff_id = %s"
+        connection.execute(query, (staff_id,))
+        affected_rows = connection.rowcount
+        if affected_rows > 0:
+            flash("Successfully recover the staff!", "success")
+            return redirect(url_for('staff_list'))
+        else:
+            flash("Recover failed. Please try again.", "danger")
+    return redirect(url_for('staff_list'))
 
 
 #Change password for all roles
