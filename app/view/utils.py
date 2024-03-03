46e501c4555a9843a1449440ab00a3f7afdda635
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-03-03 21:44:10 +1300

    Update utils.py
    Optimize weed data handling

--- app/view/utils.py
+++ app/view/utils.py
@@ -19,23 +19,24 @@ def getCursor():
     dbconn = connection.cursor()
     return dbconn
 
-def handle_weed_data(data, imgs):
-    combined_data = []
-    for weed in data:
-        weed_data = [item.decode('utf-8') if isinstance(item, bytes) else item for item in weed]
-        primary = []
-        not_primary = []
-        for img in imgs:
-            if weed[0] == img[1]:
-                img_data = [item.decode('utf-8') if isinstance(item, bytes) else item for item in img]
-                if img[3] == 1:
-                    primary.append(img_data[2]), 
-                else:
-                    not_primary.append(img_data[2])
-        img_data = primary + not_primary
-        weed_data.append(img_data)
-        combined_data.append(weed_data)
+def handle_weed_data(weed_data):
+    combined_data = {}
+    for weed in weed_data:
+        weed_id = weed[0]
+        if weed_id not in combined_data:
+            combined_data[weed_id] = {
+                'common_name': weed[1],
+                'scientific_name': weed[2],
+                'weed_type': weed[3],
+                'description': weed[4],
+                'impacts': weed[5],
+                'control_methods': weed[6],
+                'images': [weed[8]],
+            }
+        else:
+            combined_data[weed_id]['images'].append(weed[8])
     return combined_data
+
 
 def save_image(file):
     filename = secure_filename(file.filename)
