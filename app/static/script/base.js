0d55cfed9a648bd03f15d07141cec42c33ef9be4
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-03-05 11:46:13 +1300

    Update base.js
    code optimization

--- app/static/script/base.js
+++ app/static/script/base.js
@@ -1,23 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
+    handleAlerts();
+});
+
+// Make flash messages disappear after 10 seconds
+function handleAlerts() {
     var alerts = document.querySelectorAll('.alert');
     alerts.forEach(function(alert) {
         setTimeout(function() {
             alert.style.opacity = '0';
             setTimeout(function() {
-                alert.style.display = 'none';
+                alert.remove();
             }, 600); 
-        }, 10000);
+        }, 6000);
     });
-});
+}
 
+// Toggle password visibility
 function togglePasswordVisibility(password_id) {
     var passwordInput = document.getElementById(password_id);
     var isVisibility = passwordInput.type === 'text';
     passwordInput.type = isVisibility ? 'password' : 'text';
     var toggle_icon = passwordInput.nextElementSibling.querySelector('i');
     toggle_icon.className = isVisibility ? 'fa-regular fa-eye' : 'fa-regular fa-eye-slash';
+
+    // Update aria-label or title for screen readers
+    var actionText = isVisibility ? 'Show Password' : 'Hide Password';
+    passwordInput.nextElementSibling.setAttribute('aria-label', actionText);
+    passwordInput.nextElementSibling.setAttribute('title', actionText);
 };
 
+// change password input border color based on password complexity and enable/disable submit button
+function validatePassword(password_id) {
+    var passwordInput = document.getElementById(password_id);
+    var submitButton = passwordInput.form.querySelector('button[type="submit"]');
+    var pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$/;
+    
+    if (pattern.test(passwordInput.value)) {
+        passwordInput.style.borderColor = 'green';
+        submitButton.disabled = false;
+    } else {
+        passwordInput.style.borderColor = 'red';
+        submitButton.disabled = true;
+    }
+}
+
+// Preview image before upload
 function previewImg(event) {
     var files = event.target.files;
     var previewContainer = event.target.getAttribute('data-preview')
@@ -25,6 +52,7 @@ function previewImg(event) {
     imgPreviewContainer.innerHTML = '';
 
     for (var i = 0; i < files.length; i++) {
+        // only process image files
         if (!files[i].type.match('image.*')) {
             continue;
         }
@@ -33,11 +61,12 @@ function previewImg(event) {
             return function(e) {
                 var imgDiv = document.createElement('div');
                 imgDiv.classList.add('col-3', 'my-2');
-
+                // create img element
                 var img = document.createElement('img');
                 img.classList.add('img-fluid', 'rounded', 'border', 'shadow-sm');
                 img.src = e.target.result;
                 img.alt = theFile.name;
+                // append img to father div
                 imgDiv.appendChild(img);
                 imgPreviewContainer.appendChild(imgDiv);
             };
@@ -46,40 +75,31 @@ function previewImg(event) {
     }
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-    var addModal = document.getElementById('addWeedModal')
-
-    addModal.addEventListener('hidden.bs.modal', function() {
-        var form = addModal.querySelector('form');
+// reset form after modal is closed
+function resetForm(modal_id) {
+    var modal = document.getElementById(modal_id);
+    var form = modal.querySelector('form')
+    if (form) {
         form.reset();
+    }
+    // reset weed image preview
+    var primaryImgPreview = modal.querySelector('[id^=primary-image-preview]')
+    var moreImgPreview = modal.querySelector('[id^=more-image-preview]')
+    var updateMoreImgPreview = modal.querySelectorAll('[id^=update-more-image-preview]')
+    if (primaryImgPreview) {
+        primaryImgPreview.innerHTML = '';
+    }
+    if (moreImgPreview) {
+        moreImgPreview.innerHTML = '';
+    }
+    if (updateMoreImgPreview) {
+        updateMoreImgPreview.forEach(function(preview) {
+            preview.innerHTML = '';
+        })
+    }
+}
 
-        var primaryImgPreview = addModal.querySelector('[id^=primary-image-preview]')
-        var moreImgPreview = addModal.querySelector('[id^=more-image-preview]')
-        if (primaryImgPreview) {
-            primaryImgPreview.innerHTML = '';
-        }
-        if (moreImgPreview) {
-            moreImgPreview.innerHTML = '';
-        }
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    var updateModal = document.querySelectorAll('[id^=updateWeedModal]')
-
-    updateModal.forEach(function(modal) {
-        modal.addEventListener('hidden.bs.modal', function() {
-            var form = modal.querySelector('form');
-            form.reset();
-
-            var moreImgPreview = modal.querySelector('[id^=update-more-image-preview]')
-            if (moreImgPreview) {
-                moreImgPreview.innerHTML = '';
-            }
-        });
-    });
-});
-
+// mark image for deletion
 function markForDeletion(imageName, parentIndex, index) {
     var currentImage = document.getElementById('images_to_delete' + parentIndex);
     if (currentImage.value) {
