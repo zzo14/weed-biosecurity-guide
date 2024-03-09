bece595d8df7e8c436fdc341316227492d80be76
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-03-10 10:23:00 +1300

    Update home_view.py
    formatting by black

--- app/view/home_view.py
+++ app/view/home_view.py
@@ -5,16 +5,17 @@ from flask import url_for
 from flask import session
 from flask import Blueprint
 
-home_bp = Blueprint('home', __name__)
+home_bp = Blueprint("home", __name__)
 
-#Home
+
+# Home
 @home_bp.route("/")
 def home():
-    if 'loggedin' in session:
-        if session['userType'] == 'Gardener':
-            return redirect(url_for('user.gardener_profile'))
-        elif session['userType'] == 'Staff':
-            return redirect(url_for('admin_staff.staff_profile'))
-        elif session['userType'] == 'Admin':
-            return redirect(url_for('admin_staff.admin_profile'))
+    if "loggedin" in session:
+        if session["userType"] == "Gardener":
+            return redirect(url_for("user.gardener_profile"))
+        elif session["userType"] == "Staff":
+            return redirect(url_for("admin_staff.staff_profile"))
+        elif session["userType"] == "Admin":
+            return redirect(url_for("admin_staff.admin_profile"))
     return render_template("home.html")
