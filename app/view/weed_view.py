668b7c77e19f251bf0574d548baa3b7fe86642bb
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-03-10 10:23:08 +1300

    Update weed_view.py
    formatting by black

--- app/view/weed_view.py
+++ app/view/weed_view.py
@@ -14,15 +14,17 @@ from app.view.utils import save_image
 from app.view.utils import allowed_file
 import os
 
-weed = Blueprint('weed', __name__)
+weed = Blueprint("weed", __name__)
+
 
 @weed.before_request
 def before_request():
-    if 'loggedin' not in session and request.endpoint in ['weed.weed_guide']:
-        return redirect(url_for('home.home'))
+    if "loggedin" not in session and request.endpoint in ["weed.weed_guide"]:
+        return redirect(url_for("home.home"))
+
 
 # Weed guide functions
-@weed.route('/weed_guide')
+@weed.route("/weed_guide")
 def weed_guide():
     # fetch the weed guide data from the database
     profile_url = url_select()
@@ -35,35 +37,62 @@ def weed_guide():
             flash("No weeds found in the database.", "info")
     except Exception as e:
         flash(f"Database error: {e}", "danger")
-        return redirect(url_for('home.home'))
+        return redirect(url_for("home.home"))
     weed_guide = handle_weed_data(weed_data)
-    return render_template("weed_guide.html", username=session['username'], userType=session['userType'], profile_url=profile_url, weed_guide=weed_guide)
+    return render_template(
+        "weed_guide.html",
+        username=session["username"],
+        userType=session["userType"],
+        profile_url=profile_url,
+        weed_guide=weed_guide,
+    )
 
-@weed.route('/weed_guide/add_new_weed', methods=['GET', 'POST'])
+
+@weed.route("/weed_guide/add_new_weed", methods=["GET", "POST"])
 def add_new_weed():
     # add a new weed to the database
     connection = getCursor()
-    if request.method == 'POST':
-        common_name = request.form.get('common_name')
-        scientific_name = request.form.get('scientific_name')
-        weed_type = request.form.get('weed_type')
-        description = request.form.get('description')
-        impacts = request.form.get('impacts')
-        control_methods = request.form.get('control_methods')
-        primary_image = request.files.get('primary_image')
-        more_images = request.files.getlist('more_image')
+    if request.method == "POST":
+        common_name = request.form.get("common_name")
+        scientific_name = request.form.get("scientific_name")
+        weed_type = request.form.get("weed_type")
+        description = request.form.get("description")
+        impacts = request.form.get("impacts")
+        control_methods = request.form.get("control_methods")
+        primary_image = request.files.get("primary_image")
+        more_images = request.files.getlist("more_image")
 
-        if not (common_name and scientific_name and weed_type and description and impacts and control_methods and primary_image):
+        if not (
+            common_name
+            and scientific_name
+            and weed_type
+            and description
+            and impacts
+            and control_methods
+            and primary_image
+        ):
             flash("Please fill out the form!", "danger")
-            return redirect(url_for('weed.weed_guide'))
+            return redirect(url_for("weed.weed_guide"))
         # check if the file is image
-        if not allowed_file(primary_image) or (more_images[0].filename != "" and not allowed_file(more_images)):
+        if not allowed_file(primary_image) or (
+            more_images[0].filename != "" and not allowed_file(more_images)
+        ):
             flash("Please upload valid images.", "danger")
-            return redirect(url_for('weed.add_new_weed'))
+            return redirect(url_for("weed.add_new_weed"))
         try:
             # insert the weed data to the weedguide table
             query = "INSERT INTO weedguide (common_name, scientific_name, weed_type, description, impacts, control_methods) VALUES (%s, %s, %s, %s, %s, %s)"
-            connection.execute(query, (common_name, scientific_name, weed_type, description, impacts, control_methods))
+            connection.execute(
+                query,
+                (
+                    common_name,
+                    scientific_name,
+                    weed_type,
+                    description,
+                    impacts,
+                    control_methods,
+                ),
+            )
             new_id = connection.lastrowid
             affected_rows = connection.rowcount
 
@@ -82,46 +111,67 @@ def add_new_weed():
                 affected_rows = connection.rowcount
                 if affected_rows > 0:
                     flash("Successfully add a new weed! ", "success")
-                    return redirect(url_for('weed.weed_guide'))
+                    return redirect(url_for("weed.weed_guide"))
             else:
                 flash("Add failed. Please try again.", "danger")
         except Exception as e:
             flash(f"Error: {e}.Add failed. Please try again.", "danger")
-    return redirect(url_for('weed.weed_guide'))
+    return redirect(url_for("weed.weed_guide"))
 
-@weed.route('/weed_guide/update_weed/<int:weed_id>', methods=['GET', 'POST'])
+
+@weed.route("/weed_guide/update_weed/<int:weed_id>", methods=["GET", "POST"])
 def update_weed(weed_id):
     # update the weed data in the database
     connection = getCursor()
-    if request.method == 'POST':
-        common_name = request.form.get('common_name')
-        scientific_name = request.form.get('scientific_name')
-        weed_type = request.form.get('weed_type')
-        description = request.form.get('description')
-        impacts = request.form.get('impacts')
-        control_methods = request.form.get('control_methods')
-        primary_image = request.form.get('set_primary_image')
-        more_images = request.files.getlist('update_more_image')
+    if request.method == "POST":
+        common_name = request.form.get("common_name")
+        scientific_name = request.form.get("scientific_name")
+        weed_type = request.form.get("weed_type")
+        description = request.form.get("description")
+        impacts = request.form.get("impacts")
+        control_methods = request.form.get("control_methods")
+        primary_image = request.form.get("set_primary_image")
+        more_images = request.files.getlist("update_more_image")
         images_to_delete = request.form.get("images_to_delete")
-        
-        if not (common_name and scientific_name and weed_type and description and impacts and control_methods and primary_image):
+
+        if not (
+            common_name
+            and scientific_name
+            and weed_type
+            and description
+            and impacts
+            and control_methods
+            and primary_image
+        ):
             flash("Please fill out the form!", "danger")
-            return redirect(url_for('weed.weed_guide'))
+            return redirect(url_for("weed.weed_guide"))
         # check if the file is image
         if more_images[0].filename != "" and not allowed_file(more_images):
             flash("Please upload valid images.", "danger")
-            return redirect(url_for('weed.add_new_weed'))
-        
+            return redirect(url_for("weed.add_new_weed"))
+
         try:
             # if the primary image is changed to another image
             query = "UPDATE weedimage SET is_primary=0 WHERE weed_id=%s AND is_primary=1 AND image_name!=%s"
-            connection.execute(query, (weed_id, primary_image,))
+            connection.execute(
+                query,
+                (
+                    weed_id,
+                    primary_image,
+                ),
+            )
             affected_rows = connection.rowcount
             if affected_rows > 0:
                 # primary has changed, so update the new image as primary
                 query = "UPDATE weedimage SET is_primary=1 WHERE weed_id=%s AND image_name=%s"
-                connection.execute(query, (weed_id, primary_image,))
-            #insert the new images to the weedimage table
+                connection.execute(
+                    query,
+                    (
+                        weed_id,
+                        primary_image,
+                    ),
+                )
+            # insert the new images to the weedimage table
             query = "INSERT INTO weedimage (weed_id, image_name, is_primary) VALUES (%s, %s, %s)"
             try:
                 for image in more_images:
@@ -131,30 +181,50 @@ def update_weed(weed_id):
             except:
                 pass
 
-            # update the weed data in the weedguide table 
+            # update the weed data in the weedguide table
             query = "UPDATE weedguide SET common_name=%s, scientific_name=%s, weed_type=%s, description=%s, impacts=%s, control_methods=%s WHERE weed_id=%s"
-            connection.execute(query, (common_name, scientific_name, weed_type, description, impacts, control_methods, weed_id))
+            connection.execute(
+                query,
+                (
+                    common_name,
+                    scientific_name,
+                    weed_type,
+                    description,
+                    impacts,
+                    control_methods,
+                    weed_id,
+                ),
+            )
             if images_to_delete:
-                images_to_delete_list = images_to_delete.split(',')
+                images_to_delete_list = images_to_delete.split(",")
                 # only delete a image if it is not primary
                 query = "DELETE FROM weedimage WHERE weed_id = %s AND image_name = %s AND is_primary=0"
                 for image_name in images_to_delete_list:
-                    connection.execute(query, (weed_id, image_name,))
+                    connection.execute(
+                        query,
+                        (
+                            weed_id,
+                            image_name,
+                        ),
+                    )
                     affected_rows = connection.rowcount
                     if affected_rows > 0:
                         # delete the images from the static folder
-                        img_path = os.path.join(current_app.config['UPLOAD_FOLDER'], image_name)
+                        img_path = os.path.join(
+                            current_app.config["UPLOAD_FOLDER"], image_name
+                        )
                         if os.path.exists(img_path):
                             os.remove(img_path)
             flash("Successfully update! ", "success")
         except Exception as e:
             flash(f"Error: {e}. Update failed. Please try again ", "danger")
-    return redirect(url_for('weed.weed_guide'))
+    return redirect(url_for("weed.weed_guide"))
 
-@weed.route('/weed_guide/delete_weed/<int:weed_id>', methods=['GET', 'POST'])
+
+@weed.route("/weed_guide/delete_weed/<int:weed_id>", methods=["GET", "POST"])
 def delete_weed(weed_id):
     connection = getCursor()
-    if request.method == 'POST':
+    if request.method == "POST":
         # get the images name which are going to be deleted from database
         query = "SELECT image_name FROM weedimage WHERE weed_id = %s"
         connection.execute(query, (weed_id,))
@@ -166,11 +236,11 @@ def delete_weed(weed_id):
         if affected_rows > 0:
             for image in images:
                 # delete the images from the static folder
-                img_path = os.path.join(current_app.config['UPLOAD_FOLDER'], image[0])
+                img_path = os.path.join(current_app.config["UPLOAD_FOLDER"], image[0])
                 if os.path.exists(img_path):
                     os.remove(img_path)
             flash("Successfully delete the weed!", "success")
-            return redirect(url_for('weed.weed_guide'))
+            return redirect(url_for("weed.weed_guide"))
         else:
             flash("Delete failed. Please try again.", "danger")
-    return redirect(url_for('weed.weed_guide'))
+    return redirect(url_for("weed.weed_guide"))
