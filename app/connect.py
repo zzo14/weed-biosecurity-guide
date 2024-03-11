0155429aff8627e80ef5c028d45fc9a982b7dbfc
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-03-12 11:53:29 +1300

    Update connect.py

--- app/connect.py
+++ app/connect.py
@@ -1,8 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
-dbuser = "root" # Your MySQL username - likely 'root'
-dbpass = "dbPassword" # ---- PUT YOUR PASSWORD HERE ----
-dbhost = "localhost" 
+dbuser = "root"  # Your MySQL username - likely 'root'
+dbpass = "Patrickzou123"  # ---- PUT YOUR PASSWORD HERE ----
+dbhost = "localhost"
 dbport = "3306"
 dbname = "biosercurity"
-
