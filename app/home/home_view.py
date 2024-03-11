9650a89780b94d1b7673a6c9c084e110a263fde8
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-03-12 11:53:33 +1300

    Create home_view.py

---
+++ app/home/home_view.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import url_for
+from flask import session
+from flask import Blueprint
+
+home_bp = Blueprint("home", __name__, template_folder="templates")
+
+
+# Home
+@home_bp.route("/")
+def home():
+    if "loggedin" in session:
+        if session["userType"] == "Gardener":
+            return redirect(url_for("user.gardener_profile"))
+        elif session["userType"] == "Staff":
+            return redirect(url_for("admin_staff.staff_profile"))
+        elif session["userType"] == "Admin":
+            return redirect(url_for("admin_staff.admin_profile"))
+    return render_template("home.html")
