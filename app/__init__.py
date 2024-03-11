a120b2daaf477619acef93130dbbe6721b7d1781
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-03-12 11:52:48 +1300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from datetime import timedelta
 import os
 
+from app.admin_staff import admin_staff_view
+from app.auth import auth_view
+from app.home import home_view
+from app.user import user_view
+
 
 def create_app():
     app = Flask(__name__)
 
-    from app.view import home_view, auth_view, user_view, admin_staff_view, weed_view
+    from app.weed import weed_view
 
     app.secret_key = "key"
     app.permanent_session_lifetime = timedelta(hours=24)
@@ -14,9 +19,9 @@ def create_app():
     app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
     app.register_blueprint(home_view.home_bp)
-    app.register_blueprint(auth_view.auth, url_prefix="/auth")
-    app.register_blueprint(user_view.user, url_prefix="/user")
-    app.register_blueprint(admin_staff_view.admin_staff, url_prefix="/admin_staff")
-    app.register_blueprint(weed_view.weed, url_prefix="/weed")
+    app.register_blueprint(auth_view.auth_bp, url_prefix="/auth")
+    app.register_blueprint(user_view.user_bp, url_prefix="/user")
+    app.register_blueprint(admin_staff_view.admin_staff_bp, url_prefix="/admin_staff")
+    app.register_blueprint(weed_view.weed_bp, url_prefix="/weed")
 
     return app
